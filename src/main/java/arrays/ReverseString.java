3ed7fe8cd9fbba70d40848053a62a752199f6486
aayambhatt <aayambhatt23@gmail.com>
2025-12-03 21:01:00 +0530

    Reverse String

---
+++ src/main/java/arrays/ReverseString.java
@@ -0,0 +1,26 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class ReverseString {
+    static void reverseString(char[] s) {
+        int n = s.length;
+        int i = 0;
+        int j = n-1;
+
+        while(i<j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] s = {'h','e','l','l','o'};
+        reverseString(s);
+        System.out.println(Arrays.toString(s));
+
+    }
+}
